0390222f6156081af0bf5fd23a8e7f25ec4028e3
Anuj Tanwar <91279830+anujtanwar98@users.noreply.github.com>
2025-01-18 21:19:33 -0500

    Added text image button

--- idm362-at3459/ContentView.swift
+++ idm362-at3459/ContentView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var buttonTapped = false
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        ZStack {
+            Color.blue.opacity(0.1).edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("IDM362 Homework")
+                    .font(.custom("Avenir-Heavy", size: 24))
+                    .foregroundColor(.blue)
+                    .padding(10)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                        .fill(Color.white)
+                    )
+                Image("HeadShot")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                    .shadow(radius: 7)
+                
+                Text("Anuj Tanwar")
+                    .font(.custom("Avenir-Heavy", size: 20))
+                    .foregroundColor(.red)
+                Button(action: {
+                    print("Button tapped!")
+                }) {
+                    Text("Click Me")
+                        .font(.custom("Avenir-Heavy", size: 18))
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                }
+                .padding(.top, 20)
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
