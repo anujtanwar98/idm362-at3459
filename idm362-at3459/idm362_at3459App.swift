9c3845ab5dc538ceb8dabb3a7f75187fd00473a8
Anuj Tanwar <91279830+anujtanwar98@users.noreply.github.com>
2025-01-18 20:53:54 -0500

    Initial Commit

---
+++ idm362-at3459/idm362_at3459App.swift
@@ -0,0 +1,17 @@
+//
+//  idm362_at3459App.swift
+//  idm362-at3459
+//
+//  Created by Anuj Tanwar on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct idm362_at3459App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
